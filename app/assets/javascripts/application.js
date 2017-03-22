1e15bb3a9663e68b2527d731a3edf550b29d556a
lsantobuono <luciano.santobuono@gmail.com>
2017-03-22 19:11:44 +0100

    Agregue contenedores, se puede modificarles el nombre y sus productos , no logre que ande el AddContenedores, ni que el total se actualice cuando cambias los productos en el select

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,91 @@
 //= require angular
 //= require_tree .
 
-receta = angular.module('receta',[
-])
+receta = angular.module('receta',[]);
+
+receta.controller('myCtrl' , function($scope) {
+
+    $scope.all_products = [{
+        id:1,
+        name: "Producto1",
+        precio: 20,
+    },
+    {
+        id:2,
+        name: "Producto2",
+        precio: 40,
+    },
+    {
+        id:3,
+        name: "Producto3",
+        precio: 5,
+    }];
+
+    $scope.contenedores = [{
+        name: "contenedor #1",
+        editing: false,
+        displaying: false,
+        productos: [
+            $scope.all_products[0] ,$scope.all_products[1]
+        ]
+    }, {
+        name: "contenedor #2",
+        editing: false,
+        displaying: false,
+        productos: [
+            $scope.all_products[2] ,$scope.all_products[1]
+        ]
+    }, {
+        name: "contenedor #3",
+        editing: false,
+        displaying: false,
+        productos: [
+            $scope.all_products[1] ,$scope.all_products[0]
+        ]
+    }];
+
+
+    $scope.addContenedor = function ($scope) {
+        console.log("asd");
+        var i = {name: "Nuevo Contenedor", editing: false};
+        $scope.contenedores.push(i);
+        console.log(contenedores);
+    };
+
+    $scope.sumarPrecios = function(contenedor) {
+        var sum = 0;
+        for (var i = 0; i < contenedor.productos.length; i++) {
+            sum += contenedor.productos[i].precio;
+        }
+        return sum;
+    };
+
+    $scope.editContenedor = function (contenedor) {
+        contenedor.editing = true;
+    };
+
+    $scope.doneEditing = function (contenedor) {
+        contenedor.editing = false;
+        //TODO Pegarle al backend y guardar
+    };
+
+    $scope.displayProductos = function (contenedor) {
+        contenedor.displaying = true;
+    }
+
+});
+
+receta.directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
+
